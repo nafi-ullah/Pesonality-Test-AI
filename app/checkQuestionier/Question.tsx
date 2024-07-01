5b3ade529bbaf818c2b16146e9decb7b11980022
codermehraj <codermehraj@gmail.com>
2024-07-01 19:30:51 +0600

    added ques add page

---
+++ app/checkQuestionier/Question.tsx
@@ -0,0 +1,23 @@
+export const Question = () => {
+  return (
+    <div className="bg-[#180721] p-4 rounded-xl shadow-xl">
+      <h2 className="text-white text-lg mt-2 font-semibold text-center">
+        Question: How do you handle pressure?
+      </h2>
+      <div className="flex flex-wrap mt-6 gap-3 mb-2 justify-center">
+        <div className="text-white bg-response-gradient px-4 py-2 rounded-xl">
+          Option 1 Option 1
+        </div>
+        <div className="text-white bg-response-gradient px-4 py-2 rounded-xl">
+          Option 1 
+        </div>
+        <div className="text-white bg-response-gradient px-4 py-2 rounded-xl">
+          Option 1 Option 1
+        </div>
+        <div className="text-white bg-response-gradient px-4 py-2 rounded-xl">
+          Option 1 Option 1
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/checkQuestionier/page.tsx
+++ app/checkQuestionier/page.tsx
@@ -1,31 +1,29 @@
 import Navber from "@/components/Navbar";
 import Link from "next/link";
+import { Question } from "./Question";
+import { SiTestcafe } from "react-icons/si";
 
 const page = () => {
   return (
     <main className="bg-[#141416] h-screen w-screen overflow-auto flex flex-col main-scrollbar-hide">
       <Navber />
-      <div className="my-6 mx-4 sm:mx-6 md:mx-8 lg:mx-12 p-6 dashboard-bg rounded-xl shadow-xl flex-grow flex flex-col overflow-scroll main-scrollbar-hide">
-        <div className="flex-grow mt-6 px-4 flex flex-col justify-center items-center text-white text-xl">
-          <h1 className="text-left text-white text-3xl font-bold tracking-wide mb-8">
-            CHECK QUESTIONIER
-          </h1>
-          <h1>Job Title</h1>
-          <input
-            type="text"
-            className="w-full max-w-[600px] h-12 mt-2 p-2 bg-transparent border-b-2 focus:bg-[#ffffff15] focus:outline-0 focus:rounded-lg text-center"
-          />
-          <h1 className="mt-4">Description</h1>
-          <textarea className="w-full max-w-[600px] h-32 mt-2 p-2 bg-transparent border-b-2 focus:bg-[#ffffff15] focus:outline-0 focus:rounded-lg text-center" />
-          <Link
-            href={"checkQuestionier"}
-            className="text-slate-900 font-bold text-lg bg-slate-300 px-6 py-2.5 rounded-xl shadow-lg hover:bg-slate-900 hover:text-slate-200 mt-8"
-          >
-            GENERATE QUESTION
-          </Link>
+      <div className="my-6 mx-4 sm:mx-6 md:mx-8 lg:mx-12 p-6 dashboard-bg rounded-xl shadow-xl flex-grow flex flex-col items-center overflow-scroll main-scrollbar-hide">
+        <h1 className="text-yellow-300 text-3xl font-bold tracking-widest my-6 mt-4 text-center">
+          QUESTIONIER
+        </h1>
+        <div className="flex flex-col gap-4 mt-4">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <Question key={index} />
+          ))}
         </div>
+        <button className="mt-8 px-4 py-3 hover:bg-submit-gradient text-[#180721] font-bold rounded-lg text-xl max-w-[800px] w-full bg-gradient-to-r from-blue-200 to-cyan-200">
+          <div className="flex items-center justify-center">
+            CONFIRM QUESTIONIER
+            <SiTestcafe className="ml-2" />
+          </div>
+        </button>
       </div>
     </main>
-  )
-}
-export default page
+  );
+};
+export default page;
