7872004b66f64d9fc25c9a0b1d7dd3cbce79a1c8
codermehraj <codermehraj@gmail.com>
2024-07-01 16:46:05 +0600

    added response page

--- app/responses/[code]/ResponseTitleCard.tsx
+++ app/responses/[code]/ResponseTitleCard.tsx
@@ -1,16 +1,21 @@
 import { MdDelete } from "react-icons/md";
 
-type PropType = { isSelected: boolean };
+type PropType = { isSelected: boolean, onClick: () => void};
 
 export const ResponseTitleCard = (props: PropType) => {
   return (
-    <button className="mt-4 bg-response-gradient hover:bg-selected-response-gradient hover:cursor-pointer hover:scale-105 transition duration-300 ease-in-out py-2 rounded-2xl shadow-xl flex px-6 items-center gap-2 w-full"
-    style={
-      props.isSelected
-        ? { backgroundImage: "linear-gradient(to right, #41295a,  #2F0743)", scale: "1.05"}
-        : {  }
-
-    }>
+    <button
+      onClick={props.onClick}
+      className="mt-4 bg-response-gradient hover:bg-selected-response-gradient hover:cursor-pointer hover:scale-105 transition duration-300 ease-in-out py-2 rounded-2xl shadow-xl flex px-6 items-center gap-2 w-full"
+      style={
+        props.isSelected
+          ? {
+              backgroundImage: "linear-gradient(to right, #41295a,  #2F0743)",
+              scale: "1.03",
+            }
+          : {}
+      }
+    >
       <div className="bg-[#180721] text-[#ffff00] h-8 w-8 rounded-full flex justify-center items-center font-bold text-sm">
         1
       </div>
@@ -19,8 +24,22 @@ export const ResponseTitleCard = (props: PropType) => {
           Mehraj
         </h2>
       </div>
-      <button className="text-[#da2d2d] bg-[#24070e] hover:scale-125 h-8 w-8 rounded-full shadow-2xl shadow-white flex justify-center items-center font-bold text-xl">
-        <MdDelete size={16} />
+      <button
+        className="hover:scale-125 h-8 w-8 rounded-full shadow-2xl shadow-white flex justify-center items-center font-bold text-xl"
+        style={
+          props.isSelected
+            ? {
+                background: "#fff700",
+                color: "#410d19",
+                scale: "1.05",
+              }
+            : {
+                background: "#410d19",
+                color: "#fd3939",
+              }
+        }
+      >
+        <MdDelete size={props.isSelected ? 20 : 16} />
       </button>
     </button>
   );
